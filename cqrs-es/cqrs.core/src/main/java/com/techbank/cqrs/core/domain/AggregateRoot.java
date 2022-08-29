c5d8e08caf7bbe949ef7c5dbd61d45b9b88aa853
Adilkhan_Kaikenov <adilkhan_kaikenov@epam.com>
2022-08-29 12:59:32 +0600

    Event Store

--- cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/AggregateRoot.java
+++ cqrs-es/cqrs.core/src/main/java/com/techbank/cqrs/core/domain/AggregateRoot.java
@@ -30,7 +30,7 @@ public abstract class AggregateRoot {
     return this.changes;
   }
 
-  public void markChangesUncommitted() {
+  public void markChangesAsCommitted() {
     this.changes.clear();
   }
 
